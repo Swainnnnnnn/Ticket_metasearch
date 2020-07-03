1d0a61b06d9bf459b0a78729e0cb1e1bf19507a6
Swainnnnnnn <2596021916@qq.com>
2020-07-03 10:05:07 +0800

    first commit

---
+++ src/main/java/com/team06/ticketmetasearch/TicketMetasearchApplication.java
@@ -0,0 +1,13 @@
+package com.team06.ticketmetasearch;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TicketMetasearchApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TicketMetasearchApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/team06/ticketmetasearch/TicketMetasearchApplicationTests.java
@@ -0,0 +1,13 @@
+package com.team06.ticketmetasearch;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TicketMetasearchApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
