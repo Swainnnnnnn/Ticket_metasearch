bded12569d67f5678ab9ad94ac75fb45c7515650
precipitation0224 <1273196803@qq.com>
2020-07-06 18:28:11 +0800

    换正则传参

--- src/main/java/com/team06/ticketmetasearch/controller/RecommendedController.java
+++ src/main/java/com/team06/ticketmetasearch/controller/RecommendedController.java
@@ -2,19 +2,18 @@ package com.team06.ticketmetasearch.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
-import org.springframework.web.bind.annotation.RestController;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import java.sql.PreparedStatement;
 import java.sql.SQLException;
 import java.util.List;
 import java.util.Map;
 
-@RestController
+@Controller
 @RequestMapping(value = "/recommended")
 public class RecommendedController {
     @Autowired
@@ -24,28 +23,37 @@ public class RecommendedController {
     @RequestMapping(value = "/matchedInfo")
     @ResponseBody
     public List<Map<String, Object>> AirlineInfo(HttpServletRequest request, HttpServletResponse response,
-                                                 @RequestParam(value = "departureDate")String departureDate,
-                                                 @RequestParam(value = "departureCityName")String departureCityName,
-                                                 @RequestParam(value = "arrivalCityName")String arrivalCityName) throws SQLException {
+                                                 @RequestParam(value = "departureDate") String departureDate,
+                                                 @RequestParam(value = "departureCityName") String departureCityName,
+                                                 @RequestParam(value = "arrivalCityName") String arrivalCityName) throws SQLException {
 //        List<Map<String, Object>> resList = new ArrayList<Map<String, Object>>();
-        PreparedStatement statement = null;
 //        ResultSet resultSet = null;
-        String querySQL = "SELECT * FROM dwd_scheduledflight where departureDate = ? and departureCityName = ? and arrivalCityName = ?;";
-        statement.setString(1,departureDate);
-        statement.setString(2,departureCityName);
-        statement.setString(3,arrivalCityName);
-//        resultSet = statement.executeQuery();
-//        // 处理查询结果（将查询结果转换成List<Map>格式
-//        ResultSetMetaData rsmd = resultSet.getMetaData();
-//        while(resultSet.next()) {
-//            Map map = new HashMap();
-//            for(int i = 0; i < rsmd.getColumnCount(); i++) {
-//                String columnName = rsmd.getColumnName(i+1);
-//                map.put(columnName,resultSet.getString(columnName));
-//            }
-//            resList.add(map);
-//        }
-//        return resList;
+//        PreparedStatement statement = null;
+//        String querySQL = "SELECT * FROM dwd_scheduledflight where departureDate = ? and departureCityName = ? and arrivalCityName = ?;";
+//        statement.setString(1, departureDate);
+//        statement.setString(2, departureCityName);
+//        statement.setString(3, arrivalCityName);
+//        System.out.println(departureDate+" "+departureCityName+" "+arrivalCityName);
+        String querySQL = "SELECT * FROM dwd_scheduledflight where departureDate = '" + departureDate +
+                "' and departureCityName = '" + departureCityName +
+                "' and arrivalCityName = '" + arrivalCityName + "';";
+        System.out.println(departureDate+" "+departureCityName+" "+arrivalCityName);
+        return (List<Map<String, Object>>) jdbcTemplate.queryForList(querySQL);
+    }
+
+    // 指定航空公司
+    @RequestMapping(value = "/chosenInfo")
+    @ResponseBody
+    public List<Map<String, Object>> AirChosenInfo(HttpServletRequest request, HttpServletResponse response,
+                                                   @RequestParam(value = "departureDate") String departureDate,
+                                                   @RequestParam(value = "departureCityName") String departureCityName,
+                                                   @RequestParam(value = "arrivalCityName") String arrivalCityName,
+                                                   @RequestParam(value = "airlineName") String airlineName) throws SQLException {
+        String querySQL = "SELECT * FROM dwd_scheduledflight where departureDate = '" + departureDate +
+                "' and departureCityName = '" + departureCityName +
+                "' and arrivalCityName = '" + arrivalCityName +
+                "' and airlineName = '" + "';";
+        System.out.println(departureDate+" "+departureCityName+" "+arrivalCityName+" "+airlineName);
         return (List<Map<String, Object>>) jdbcTemplate.queryForList(querySQL);
     }
 }
